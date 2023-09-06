89c3b7d0d14314fd1f57ddcefd719dedf4af5e08
DZ-FSDev <dz.uofm@gmail.com>
2023-09-06 15:56:21 -0500

    Initial Commit

---
+++ app/src/main/java/com/rrc/adev3007/pixel_perfect/the_y_app/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.rrc.adev3007.pixel_perfect.the_y_app.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
