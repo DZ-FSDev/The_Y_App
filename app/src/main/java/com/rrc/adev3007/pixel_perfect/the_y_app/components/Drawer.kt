1ab46ea1845272b5ff3381fe846143cbb5f4f2ff
DZ-FSDev <dz.uofm@gmail.com>
2023-09-22 21:55:00 -0600

    Update Drawer.kt

--- app/src/main/java/com/rrc/adev3007/pixel_perfect/the_y_app/components/Drawer.kt
+++ app/src/main/java/com/rrc/adev3007/pixel_perfect/the_y_app/components/Drawer.kt
@@ -4,22 +4,25 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import com.rrc.adev3007.pixel_perfect.the_y_app.session.Session
+import androidx.compose.ui.platform.LocalContext
+
 
 object DrawerState {
     var isDrawerOpen by mutableStateOf(false)
@@ -31,44 +34,83 @@ object DrawerState {
 
 @Composable
 fun Drawer() {
+    var session = Session.getInstance(LocalContext.current);
+
     val isDrawerOpen = DrawerState.isDrawerOpen
     AnimatedVisibility(
-        visible = isDrawerOpen,
-        enter = slideInHorizontally(initialOffsetX = { -it }),
-        exit = slideOutHorizontally(targetOffsetX = { -it }),
+            visible = isDrawerOpen,
+            enter = slideInHorizontally(initialOffsetX = { -it }),
+            exit = slideOutHorizontally(targetOffsetX = { -it }),
     ) {
+        var darkMode by remember { mutableStateOf(session!!.getBoolean("darkMode", false)) }
+        var autoplay by remember { mutableStateOf(session!!.getBoolean("autoplay",false)) }
+        var profanityFilter by remember { mutableStateOf(session!!.getBoolean("profanityFilter",false)) }
+        var scale by remember {
+            mutableStateOf(
+                ScalingLevel.valueOf(session?.getString("scale", ScalingLevel.Normal.toString()) ?: ScalingLevel.Normal.toString())
+            )
+        }
+
+
         Column(
-            modifier = Modifier
-                .zIndex(1f)
-                .fillMaxHeight()
-                .width(250.dp)
-                .background(Color.DarkGray)
+                modifier =
+                        Modifier.zIndex(100f)
+                                .fillMaxHeight()
+                                .fillMaxWidth(0.5f)
+                                .background(
+                                        if (darkMode) Color.hsv(0f, 0f, 0.1f, 1f)
+                                        else Color.Gray
+                                )
         ) {
-            Text(
-                text = "Drawer Content",
-                modifier = Modifier.padding(bottom = 16.dp)
+            DefaultProfileIcon(
+                    modifier = Modifier.padding(16.dp),
+                    onClick = { DrawerState.toggleDrawer() }
             )
 
             Text(
-                text = "Item 1",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .clickable {}
+                    text = "Settings",
+                    modifier = Modifier.padding(horizontal = 19.dp, vertical = 8.dp),
+                    fontSize =
+                            if (session!!.getString("scale", "") == ScalingLevel.Small.toString()) 15.sp
+                            else if (session!!.getString("scale", "") == ScalingLevel.Normal.toString()) 20.sp
+                            else 25.sp,
+                    color =
+                            if (session!!.getBoolean("darkMode", false)) Color.White
+                            else Color.Black // Set font color
             )
-            Text(
-                text = "Item 2",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .clickable {}
+
+            ScalingSlider(
+                text = "UI Scaling",
+                initialLevel = scale,
+                onLevelChange = {
+                    scale = it
+                    session!!.putString("scale", it.toString())
+                }
             )
-            Text(
-                text = "Item 3",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-                    .clickable {}
+
+            SettingToggle(
+                    text = "Dark Mode",
+                    initialChecked = darkMode,
+                    onCheckedChange = {
+                        darkMode = it
+                        session!!.putBoolean("darkMode", it)
+                    }
+            )
+            SettingToggle(
+                    text = "Auto Play",
+                    initialChecked = autoplay,
+                    onCheckedChange = {
+                        autoplay = it
+                        session!!.putBoolean("autoplay", it)
+                    }
+            )
+            SettingToggle(
+                    text = "Profanity Filter",
+                    initialChecked = profanityFilter,
+                    onCheckedChange = {
+                        profanityFilter = it
+                        session!!.putBoolean("profanityFilter", it)
+                    }
             )
         }
     }
