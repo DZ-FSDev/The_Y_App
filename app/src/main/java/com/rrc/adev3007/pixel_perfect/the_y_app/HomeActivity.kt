fe3f73d6f2b15d5ce83127a98270ca5a49298366
DZ-FSDev <dz.uofm@gmail.com>
2023-09-23 16:17:35 -0500

    Update HomeActivity.kt

--- app/src/main/java/com/rrc/adev3007/pixel_perfect/the_y_app/HomeActivity.kt
+++ app/src/main/java/com/rrc/adev3007/pixel_perfect/the_y_app/HomeActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -32,69 +31,73 @@ import com.rrc.adev3007.pixel_perfect.the_y_app.pages.Dislikes
 import com.rrc.adev3007.pixel_perfect.the_y_app.pages.Home
 import com.rrc.adev3007.pixel_perfect.the_y_app.pages.Search
 import com.rrc.adev3007.pixel_perfect.the_y_app.ui.theme.LogoFontFamily
+import com.rrc.adev3007.pixel_perfect.the_y_app.session.Session
+import androidx.compose.ui.platform.LocalContext
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val navController = rememberNavController()
-            val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
-            Surface(
-                modifier = Modifier
-                    .fillMaxSize(),
-                color = Color.Black
-            ) {
-                Drawer()
-                Column(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                ) {
+            val currentRoute =
+                    navController.currentBackStackEntryAsState().value?.destination?.route
+            Surface(modifier = Modifier.fillMaxSize(), color = Color.Black) {
+                var session = Session.getInstance(LocalContext.current);
+
+                Column(modifier = Modifier.fillMaxSize()) {
                     Box(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        contentAlignment = Alignment.CenterStart
+                            modifier = Modifier.fillMaxWidth(),
+                            contentAlignment = Alignment.CenterStart
                     ) {
                         DefaultProfileIcon(
-                            modifier = Modifier
-                                .padding(16.dp)
-                                .align(Alignment.TopStart),
-                            onClick = { DrawerState.toggleDrawer() }
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .align(Alignment.TopStart),
+                                onClick = { DrawerState.toggleDrawer() }
                         )
                         Text(
-                            text = "y",
-                            color = Color.White,
-                            fontSize = 30.sp,
-                            fontFamily = LogoFontFamily,
-                            modifier = Modifier
-                                .align(Alignment.TopCenter)
-                                .clickable {
-                                    if (currentRoute != "Home") navController.navigate("Home")
-                                }
+                                text = "y",
+                                color = Color.White,
+                                fontSize = 30.sp,
+                                fontFamily = LogoFontFamily,
+                                modifier =
+                                Modifier
+                                    .align(Alignment.TopCenter)
+                                    .clickable {
+                                        if (currentRoute != "Home")
+                                            navController.navigate("Home")
+                                    }
                         )
                     }
-                    Spacer(
-                        modifier = Modifier
-                            .height(1.dp)
-                            .fillMaxWidth()
-                            .background(Color.White)
+
+                    Text(
+                        text = "Welcome Back " + session!!.getString("username", "Undefined")!!,
+                        /*color = if(session!!.getBoolean("darkMode", false)) Color.White
+                            else Color.Red*/ /*TODO This doesn't work... */
+                        color = Color.White,
+                        modifier = Modifier.padding(start = 10.dp)
                     )
+
+                    Spacer(modifier = Modifier
+                        .height(1.dp)
+                        .fillMaxWidth()
+                        .background(Color.White))
                     NavHost(
-                        navController,
-                        startDestination = "Home",
-                        modifier = Modifier
-                            .weight(1f)
+                            navController,
+                            startDestination = "Home",
+                            modifier = Modifier.weight(1f)
                     ) {
                         composable("Home") { Home() }
                         composable("Search") { Search() }
                         composable("Dislikes") { Dislikes() }
                     }
                     BottomNavBar(
-                        navController = navController,
-                        currentRoute = currentRoute,
-                        modifier = Modifier
-                            .padding(horizontal = 30.dp)
+                            navController = navController,
+                            currentRoute = currentRoute,
+                            modifier = Modifier.padding(horizontal = 30.dp)
                     )
                 }
+                Drawer()
             }
         }
     }
